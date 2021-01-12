afa4535be39225044a4e45bd8ca7227818626848
Nick Mathews <nick.mathews@stablekernel.com>
2021-01-12 17:43:37 -0500

    Setting up click listeners

--- app/src/main/java/com/mathewsmobile/spacebook/ui/feed/FeedAdapter.kt
+++ app/src/main/java/com/mathewsmobile/spacebook/ui/feed/FeedAdapter.kt
@@ -1,43 +1,56 @@
 package com.mathewsmobile.spacebook.ui.feed
 
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.mathewsmobile.spacebook.R
 import com.mathewsmobile.spacebook.model.FeedItem
+import java.lang.Exception
 
 class FeedAdapter : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
-    
+
     private var feedData: List<FeedItem> = listOf()
-    
+
     fun setFeedData(newFeedData: List<FeedItem>) {
+//        feedData = newFeedData.fold(mutableListOf()) { list, element ->
+//            if (element.)
+//        }
         feedData = newFeedData
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        
-        return when(viewType) {
+
+        return when (viewType) {
             NEW_POST_TYPE -> {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_new_post, parent, false)
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_new_post, parent, false)
                 NewPostViewHolder(view)
             }
             NEW_COMMENT_TYPE -> {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_new_comment, parent, false)
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_new_comment, parent, false)
                 NewCommentViewHolder(view)
             }
             GITHUB_PR_TYPE -> {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_pull_request, parent, false)
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_pull_request, parent, false)
                 PullRequestViewHolder(view)
             }
             GITHUB_PUSH_TYPE -> {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_push, parent, false)
+                val view =
+                    LayoutInflater.from(parent.context).inflate(R.layout.item_push, parent, false)
                 PushViewHolder(view)
             }
             else -> {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_feed, parent, false)
+                val view =
+                    LayoutInflater.from(parent.context).inflate(R.layout.item_feed, parent, false)
                 BasicViewHolder(view, viewType)
             }
         }
@@ -45,7 +58,7 @@ class FeedAdapter : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
 
     override fun getItemViewType(position: Int): Int {
         val item = feedData[position]
-        return when(item.type) {
+        return when (item.type) {
             FeedItem.Companion.Type.NEW_POST -> NEW_POST_TYPE
             FeedItem.Companion.Type.NEW_COMMENT -> NEW_COMMENT_TYPE
             FeedItem.Companion.Type.GITHUB_NEW_PR, FeedItem.Companion.Type.GITHUB_MERGED_PR -> GITHUB_PR_TYPE
@@ -59,10 +72,10 @@ class FeedAdapter : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = feedData[position]
-        
+
         holder.bind(item)
     }
-    
+
     companion object {
         const val NEW_POST_TYPE = 3
         const val NEW_COMMENT_TYPE = 4
@@ -73,60 +86,109 @@ class FeedAdapter : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
     abstract class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         abstract fun bind(item: FeedItem)
     }
-    
+
     class BasicViewHolder(val view: View, val viewType: Int) : ViewHolder(view) {
         override fun bind(item: FeedItem) {
             view.findViewById<TextView>(R.id.itemTitle).text = "Unknown view type $viewType"
         }
     }
-    
+
     class NewPostViewHolder(view: View) : ViewHolder(view) {
-        
-        val title: TextView = view.findViewById(R.id.newPostTitle)
-        val date: TextView = view.findViewById(R.id.newPostDate)
-        
+
+        private val title: TextView = view.findViewById(R.id.newPostTitle)
+        private val date: TextView = view.findViewById(R.id.newPostDate)
+
+        private val listItem: ConstraintLayout = view.findViewById(R.id.newPostItem)
+
         override fun bind(item: FeedItem) {
             val newPost = item.getPayload() as FeedItem.Post
-            
+
             title.text = newPost.title
             date.text = newPost.postedAt
+
+            listItem.setOnClickListener {
+                // TODO Go to the post page for the relevant post
+                Log.d("NewPostViewHolder", "Trying to view the post ${newPost.id}")
+            }
         }
     }
-    
+
     class NewCommentViewHolder(view: View) : ViewHolder(view) {
-        
-        val commentHeader: TextView = view.findViewById(R.id.newCommentTitle)
-        
+
+        private val commentHeader: TextView = view.findViewById(R.id.newCommentTitle)
+        private val commentMessage: TextView = view.findViewById(R.id.newCommentMessage)
+        private val commentDate: TextView = view.findViewById(R.id.newCommentDate)
+
+        private val listItem: ConstraintLayout = view.findViewById(R.id.newCommentItem)
+
         override fun bind(item: FeedItem) {
             val comment = item.getPayload() as FeedItem.NewComment
-            
-            commentHeader.text = "Commented on a post by user ${comment.userId}"
-        }
-    }
-    
-    class PullRequestViewHolder(view: View) : ViewHolder(view) {
-        
-        private val title: TextView = view.findViewById(R.id.pullRequestTitle)
-        
-        override fun bind(item: FeedItem) {
-            val githubPr = item.getPayload() as FeedItem.GithubPR
-            
-            if (item.type == FeedItem.Companion.Type.GITHUB_MERGED_PR) {
-                title.text = "Merged #${githubPr.pullRequestNumber} into ${githubPr.repository}"
-            } else if (item.type == FeedItem.Companion.Type.GITHUB_NEW_PR) {
-                title.text = "Opened a new pull request #${githubPr.pullRequestNumber} for ${githubPr.repository}"
+
+            commentHeader.text = "Commented on the post ${comment.postId}"
+            commentMessage.text = comment.message
+            commentDate.text = comment.commentedAt
+
+            listItem.setOnClickListener {
+                // TODO Go to the post page for the relevant post
+                Log.d(
+                    "NewCommentViewHolder",
+                    "Trying to view the comment on post ${comment.postId}"
+                )
             }
         }
     }
     
-    class PushViewHolder(view: View) : ViewHolder(view) {
-        
+    class PullRequestViewHolder(private val view: View) : ViewHolder(view) {
+
+        private val title: TextView = view.findViewById(R.id.pullRequestTitle)
+        private val date: TextView = view.findViewById(R.id.pullRequestDate)
+
+        private val listItem: ConstraintLayout = view.findViewById(R.id.pullRequestItem)
+
+        override fun bind(item: FeedItem) {
+            val githubPr = item.getPayload() as FeedItem.GithubPR
+
+            if (item.type == FeedItem.Companion.Type.GITHUB_MERGED_PR) {
+                title.text = "Merged #${githubPr.pullRequestNumber} into ${githubPr.repository}"
+            } else if (item.type == FeedItem.Companion.Type.GITHUB_NEW_PR) {
+                title.text =
+                    "Opened a new pull request #${githubPr.pullRequestNumber} for ${githubPr.repository}"
+            }
+
+            date.text = item.occurredAt
+
+            listItem.setOnClickListener {
+                val githubIntent = Intent(Intent.ACTION_VIEW, Uri.parse(githubPr.url))
+                try {
+                    view.context.startActivity(githubIntent)
+                } catch (e: Exception) {
+                    Log.e("PushViewHolder", "Unable to go to github", e)
+                }
+            }
+        }
+    }
+
+    class PushViewHolder(private val view: View) : ViewHolder(view) {
+
         private val title: TextView = view.findViewById(R.id.pushTitle)
-        
+        private val date: TextView = view.findViewById(R.id.pushDate)
+
+        private val listItem: ConstraintLayout = view.findViewById(R.id.githubPushItem)
+
         override fun bind(item: FeedItem) {
             val pushes = item.getPayload() as FeedItem.GithubPush
-            
-            title.text = "Pushed commits to ${pushes.repository} on the branch ${pushes.branch}"
+
+            title.text = "Pushed a commit to ${pushes.repository} on the branch ${pushes.branch}"
+            date.text = item.occurredAt
+
+            listItem.setOnClickListener {
+                val githubIntent = Intent(Intent.ACTION_VIEW, Uri.parse(pushes.url))
+                try {
+                    view.context.startActivity(githubIntent)
+                } catch (e: Exception) {
+                    Log.e("PushViewHolder", "Unable to go to github", e)
+                }
+            }
         }
     }
 }
